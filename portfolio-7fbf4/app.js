b0e33b4e15ecdacfdeb0a01b91ff2627881e7b06
Nisham-N-K <nishamnishamnk@gmail.com>
2024-04-18 11:09:48 +0530

    connect in firebase

---
+++ portfolio-7fbf4/app.js
@@ -0,0 +1,12 @@
+// app.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, World! This is my portfolio.');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
